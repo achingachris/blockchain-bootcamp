b0b20481c2faf4cfc2ce4a8c29cf4688fc7673f9
chrisdevcode <achinga.chris@gmail.com>
2022-06-04 09:51:02 +0300

    day1/3

---
+++ script.js
@@ -0,0 +1,76 @@
+// let user_input = prompt('Please enter something')
+
+// let user_input_type = typeof user_input
+
+// if (user_input_type === string) {
+//   for (let i = 0; i < 5; i++) {
+//     window.alert(user_input)
+//   }
+// } else if (user_input_type === undefined || user_input_type === null) {
+//   window.alert('Please enter something')
+// }
+
+let sample_text_2 = document.getElementById('sample')
+sample_text_2.innerHTML = 'Hello World'
+
+let sample_text_3 = document.querySelector('.text3')
+sample_text_3.innerHTML = 'Hello World'
+
+let sample_text_2 = document.querySelector('.text2')
+sample_text_2.innerHTML = '<h1>Hello World</h1>'
+
+const list = document.getElementsByTagName('ul')[0]
+list.getElementsByTagName('li')[0].innerHTML = 'water: hot'
+
+// let sample_text_3 = document.getElementsByClassName("text3")
+// sample_text_3.innerHTML = "Hello World"
+
+// console.log(user_input)
+// console.log(user_input_type)
+
+// debugger
+// console.
+
+// function name(params) {
+
+// }
+
+// const name = (params) => { }
+
+// let my_function = (params) => { }
+
+// ;(function () {
+//   console.log('i called myself')
+// })()
+
+// doesn't work
+// (n => { console.log("Ok"); })()
+
+// recursive function
+// function countDown(fromNumber) {
+//   console.log(fromNumber)
+
+//   let nextNumber = fromNumber - 1
+
+//   if (nextNumber > 0) {
+//     countDown(nextNumber)
+//   }
+// }
+// countDown(3)
+
+// // recursive function
+// ;(function () {
+//   let fromNumber = 1000
+//   let nextNumber = fromNumber - 1
+
+//   if (nextNumber > 0) {
+//     countDown(nextNumber)
+//   }
+// })()
+
+// //
+// function userAlert() {
+//     alert('Hello')
+// }
+
+// userAlert()
